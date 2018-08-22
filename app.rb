eb74de7f295e120a8c9ebc4e49553d72d32c3221
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-22 00:02:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,5 +7,9 @@ class App < Sinatra::Base
   
   get '/hometown' do
   "My hometown is Jackson Mississippi, that is often a security question.. dont hack me please...my fellow rubiest... MSF was built on Ruby after all.)"
-end
+  end
+  
+  get '/favorite_song' do
+  "Objectively Stairway to Heaven, because if you do not agree Led Zeppelin is the best you are so wrong... The Beatles were good, but try Since I've Been Loving You Live Madison Square Garden 1973.. haha John Lennons ass up there!"
+  end
 end
