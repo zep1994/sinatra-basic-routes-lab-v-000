a9beba39daf38f1efa2b4b91f5c23d28ff0e46cb
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-22 00:00:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,10 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-  "My name is Thomas Matlock also called Snaxbags on twitte:)"
+  "My name is Thomas Matlock also called Snaxbags on twitter)"
   end
   
   get '/hometown' do
-  "My hometown is Jackson Mississippi, that is often a security question.. dont hack me please...my fellow rubiest... MSF was built on Ruby after all.:)"
+  "My hometown is Jackson Mississippi, that is often a security question.. dont hack me please...my fellow rubiest... MSF was built on Ruby after all.)"
 end
 end
